d3a3e8549489339e4965349beb8178ddd35733de
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-31 00:43:55 -0600

    2 commit

--- Actividad_10.py
+++ Actividad_10.py
@@ -1,1 +1,9 @@
-print("prueba funcionalidad del respositorio")
+# print("prueba funcionalidad del respositorio")
+producto_nuevo= {}
+cantidad_productos = int(input("==Cuantos productos desea ingresar==: "))
+for i in range(cantidad_productos):
+    print(f"Ingresa el producto_{i+1}: ")
+    codigo=int(input(f"Ingrese Codigo del producto: "))
+    nombre=input(f"Ingrese Nombre del producto: ")
+    print(f"Ingrese Categoria del producto: ")
+
